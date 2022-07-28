af7e3e901fa941b694fc0884054e328b99c34413
jeonga-Son <jjeong0106@gmail.com>
2022-07-28 11:38:07 +0900

    json을 다시 객체화

--- src/main/java/com/ll/exam/article/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -2,10 +2,12 @@ package com.ll.exam.article.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ArticleDto {
     private long id;
 
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,13 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    void 실험_assertThat() {
+    void assertJ_assertThat() {
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
     }
 
     @Test
-    void 실험_ObjectMapper() throws JsonProcessingException {
+    void ObjectMapper_objToJsonStr() throws JsonProcessingException {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
 
         String jsonStr = Ut.json.toStr(articleDto, "");
@@ -22,5 +22,15 @@ public class AppTest {
         assertThat(jsonStr).isEqualTo("""
                 {"id":1,"title":"제목","body":"내용"}
                 """.trim());
+    }
+
+    @Test
+    void ObjectMapper__jsonStrToObj() {
+        ArticleDto articleDtoOrigin = new ArticleDto(1, "제목", "내용");
+        String jsonStr = Ut.json.toStr(articleDtoOrigin, "");
+
+        ArticleDto articleDtoFromJson = (ArticleDto) Ut.json.toObj(jsonStr, ArticleDto.class, null);
+
+        assertThat(articleDtoOrigin).isEqualTo(articleDtoFromJson);
     }
 }
