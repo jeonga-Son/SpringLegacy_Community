4bec5de8be23ed1a36c3c86f92a05a11ee49844f
jeonga-Son <jjeong0106@gmail.com>
2022-08-03 01:38:18 +0900

    JSON 응답 체계에 양식을 정함, resultCode, msg 필수

---
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -0,0 +1,42 @@
+package com.ll.exam.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
+
+
+public class Ut {
+    public static class json {
+        private static final ObjectMapper om;
+
+        static {
+            om = new ObjectMapper();
+        }
+
+        public static String toStr(Object obj, String defaultValue) {
+            try {
+                return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        //제너릭
+        public static <T> T toObj(String jsonStr, Class<T> cls, T defaultValue) {
+            try {
+                return (T)om.readValue(jsonStr, cls);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> T toObj(String jsonStr, TypeReference<T> typeReference, T defaultValue) {
+            try {
+                return om.readValue(jsonStr, typeReference);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+    }
+}
