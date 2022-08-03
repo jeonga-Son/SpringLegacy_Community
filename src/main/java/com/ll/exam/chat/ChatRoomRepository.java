f736fcf667d26544cd1163b12e82b228e062b961
jeonga-Son <jjeong0106@gmail.com>
2022-08-03 13:45:22 +0900

    채팅방 수정 처리

---
+++ src/main/java/com/ll/exam/chat/ChatRoomRepository.java
@@ -0,0 +1,59 @@
+package com.ll.exam.chat;
+
+import com.ll.exam.chat.dto.ChatRoomDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ChatRoomRepository {
+    private static List<ChatRoomDto> datum;
+    private static long lastId;
+
+    static {
+        datum = new ArrayList<ChatRoomDto>();
+        lastId = 0;
+
+        makeTestData();
+    }
+
+    private static void makeTestData() {
+        IntStream.rangeClosed(1, 10).forEach(id -> {
+            String title = "이름%d".formatted(id);
+            String body = "주제%d".formatted(id);
+            create(title, body);
+        });
+    }
+
+    public static long create(String title, String body) {
+        //나중에 사이트 데이터가 21억개가 넘을 것을 대비해서 long으로 잡음.
+        long id = ++lastId;
+        ChatRoomDto newChatRoomDto = new ChatRoomDto(id, title, body);
+
+        datum.add(newChatRoomDto);
+
+        return id;
+    }
+
+    public List<ChatRoomDto> findAll() {
+        return datum;
+    }
+
+    public ChatRoomDto findById(long id) {
+            for ( ChatRoomDto chatRoomDto : datum ) {
+                if ( chatRoomDto.getId() == id ) {
+                    return chatRoomDto;
+                }
+            }
+            return null;
+        }
+
+    public void modify(long id, String title, String body) {
+        ChatRoomDto chatRoomDto = findById(id);
+
+        chatRoomDto.setTitle(title);
+        chatRoomDto.setBody(body);
+    }
+}
+
+
---
+++ src/main/java/com/ll/exam/chat/ChatService.java
@@ -0,0 +1,30 @@
+package com.ll.exam.chat;
+
+import com.ll.exam.chat.dto.ChatRoomDto;
+
+import java.util.List;
+
+public class ChatService {
+    private ChatRoomRepository chatRoomRepository;
+
+    public ChatService() {
+        chatRoomRepository = new ChatRoomRepository();
+    }
+
+    public long createRoom(String title, String body) {
+        return chatRoomRepository.create(title, body);
+    }
+
+    public List<ChatRoomDto> findAllRooms() {
+        return chatRoomRepository.findAll();
+    }
+
+    public ChatRoomDto findRoomById(long id) {
+        return chatRoomRepository.findById(id);
+    }
+
+    public void modifyRoom(long id, String title, String body) {
+        chatRoomRepository.modify(id, title, body);
+
+    }
+}
